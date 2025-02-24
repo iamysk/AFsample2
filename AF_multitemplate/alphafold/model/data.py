0b2ace9d16c6272dea15907c078c7f325e1f77c9
Yogesh Kalakoti <ykalakoti@gmail.com>
2025-02-24 14:11:14 +0100

    Accept cfold parameters

--- AF_multitemplate/alphafold/model/data.py
+++ AF_multitemplate/alphafold/model/data.py
@@ -21,13 +21,29 @@ import haiku as hk
 import numpy as np
 # Internal import (7716).
 
+def rename_keys(d):
+    new_d = {}
+    for key, value in d.items():
+        parts = key.rsplit("/", 1)  # Split at the last "/"
+        new_key = f"{parts[0]}//{parts[1]}" if len(parts) > 1 else key
+        new_d[new_key] = rename_keys(value) if isinstance(value, dict) else value
+    return new_d
 
 def get_model_haiku_params(model_name: str, data_dir: str) -> hk.Params:
   """Get the Haiku parameters from a model name."""
 
-  path = os.path.join(data_dir, 'params', f'params_{model_name}.npz')
-
-  with open(path, 'rb') as f:
-    params = np.load(io.BytesIO(f.read()), allow_pickle=False)
-
-  return utils.flat_params_to_haiku(params)
+  if model_name=='cfold':
+    path = os.path.join(data_dir, 'params', f'params_{model_name}.npy')
+    with open(path, 'rb') as f:
+      params = np.load(io.BytesIO(f.read()), allow_pickle=True)
+    
+    params_ = rename_keys(params)
+    #print(params_.keys())
+    return utils.flat_params_to_haiku_cfold(params)
+  else:
+    path = os.path.join(data_dir, 'params', f'params_{model_name}.npz')
+    with open(path, 'rb') as f:
+      params = np.load(io.BytesIO(f.read()), allow_pickle=False)
+    return utils.flat_params_to_haiku(params)
+  
+# alphafold/alphafold_iteration/distogram_head/half_logits
